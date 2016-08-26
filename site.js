8c798f26321b6c32de389f45e986eceb50c656d2
Xiaoyu Tai <taixiaoyu@gmail.com>
2016-08-26 13:33:17 -0400

    add umd library settings.

--- site.js
+++ site.js
@@ -25,25 +25,23 @@ $(function() {
         "canvasControls": { // The types of controls available to be displayed on a canvas
           "annotations" : {
             "annotationLayer" : true, //whether or not to make annotation layer available in this window
-            // "annotationCreation" : true, 
+            "annotationCreation" : false, 
             /*whether or not to make annotation creation available in this window,
                          only valid if annotationLayer is set to True and an annotationEndpoint is defined.
                          This setting does NOT affect whether or not a user can edit an individual annotation that has already been created.*/
             "annotationState" : 'on', //[_'off'_, 'on'] whether or not to turn on the annotation layer on window load
             // "annotationRefresh" : false, //whether or not to display the refresh icon for annotations
           },
-          // "imageManipulation" : {
-          //   "manipulationLayer" : false,
-          // }
-      },
+          "imageManipulation" : {
+            "manipulationLayer" : false,
+          }
+        }
       }
     ],
     'drawingToolsSettings': {
       // Additional tool settings.
-      /**
-       *'Pin': {
-       *},
-       **/
+      // 'Pin': {
+      // },
       'selectedColor': 'red',
       // 'doubleClickReactionTime': 300,
       'strokeColor': 'yellow',
